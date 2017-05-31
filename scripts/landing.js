e7f17ebceebad08bb23b81bdaa43be83bda08a46
Edward Choi <edwardchoi.2011@gmail.com>
2017-05-31 04:03:02 -0700

    Made CP8 assignment according to solution video

--- scripts/landing.js
+++ scripts/landing.js
@@ -1,20 +1,15 @@
- var revealPoint = function() {
-
+ var animatePoints = function() {
+     
      var points = document.getElementsByClassName('point');
 
-     for(var i = 0; i < points.length; i++){
-         points[i].style.opacity = 1;
-         points[i].style.transform = "scaleX(1) translateY(0)";
-         points[i].style.msTransform = "scaleX(1) translateY(0)";
-         points[i].style.WebkitTransform = "scaleX(1) translateY(0)";
+     var revealPoint = function(index){
+         points[index].style.opacity = 1;
+         points[index].style.transform = "scaleX(1) translateY(0)";
+         points[index].style.msTransform = "scaleX(1) translateY(0)";
+         points[index].style.WebkitTransform = "scaleX(1) translateY(0)";
      }
 
-     var revealFirstPoint = points[0];
-     var revealSecondPoint = points[1];
-     var revealThirdPoint = points[2];
-
-     revealFirstPoint();
-     revealSecondPoint();
-     revealThirdPoint();
- 
+     for(var i = 0; i < points.length; i++){
+        revealPoint(i);
+     }
  };
